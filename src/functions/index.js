dbf645537311ca3e9b1aef90efcfae8b57522a30
princel3lcS <princeboy__@hotmail.com>
2018-06-27 14:23:27 +0700

    update function getLogs

--- src/functions/index.js
+++ src/functions/index.js
@@ -315,6 +315,34 @@ app.get('/removeVIP', (req, res) => {
   }
 })
 
+app.get('/getLogs', async (req, res) => {
+  const key = req.query.key
+  if (checkKey === key) {
+    let logs = []
+    let logsList = await db.collection('logs').get().then( QuerySnapshot => {
+      QuerySnapshot.forEach( async doc => {
+        await logs.push({
+          ...doc.data()
+        })
+      })
+    })
+    res.status(200).send({
+      message: 'send logs complete',
+      data: {
+        status: 200,
+        logs
+      }
+    })
+  } else {
+    res.status(200).send({
+      message: 'not authentication',
+      data: {
+        status: 200
+      }
+    })
+  }
+})
+
 function getNetDiscount (net, discountType, discountNumber) { 
   const discountNumberInt = parseInt(discountNumber)
   if (discountType === 'amount') {
