319660a9c7e31097c21549e68b25ecbc6c95ff24
princel3lcS <princeboy__@hotmail.com>
2018-07-05 10:37:19 +0700

    set constances

--- src/functions/constances/index.js
+++ src/functions/constances/index.js
@@ -1,4 +1,11 @@
 import admin from 'firebase-admin'
+const express = require('express')
+const CORS = require('cors')({origin: true})
+const app = express()
+app.use(CORS)
+admin.initializeApp({
+    credential: admin.credential.applicationDefault() 
+})
 const db = admin.firestore()
 
 const checkKey = '4784d051-dce8-4918-a0b1-9b429a23f8d6'
@@ -12,6 +19,7 @@ const state = {
 }
 
 module.exports = {
+  app,
   db,
   checkKey,
   state
--- src/functions/index.js
+++ src/functions/index.js
@@ -1,12 +1,5 @@
 import * as functions from 'firebase-functions'
-import admin from 'firebase-admin'
-const express = require('express')
-const CORS = require('cors')({origin: true})
-const app = express()
-app.use(CORS)
-admin.initializeApp({
-    credential: admin.credential.applicationDefault() 
-})
+import { app } from './constances'
 
 const { getDiscount } = require('./lib/getDiscount')
 const { checkOut } = require('./lib/checkOut')
