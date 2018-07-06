472068ccd9f9601b61cb80a61a463c71e430ea67
princel3lcS <princeboy__@hotmail.com>
2018-07-06 12:33:54 +0700

    update transaction spending for vip

--- src/functions/lib/getDiscount.js
+++ src/functions/lib/getDiscount.js
@@ -8,11 +8,8 @@ const getDiscount = async (req, res) => {
     promoCode,
     key
   }
-  // console.log(raw)
-  // console.log('checkKey = ', checkKey)
   if (checkKey === key) {
     const promotionDetails = await db.collection('promoCode').doc(promoCode).get()
-    // console.log('db = ', promotionDetails.exists)
     if (promotionDetails.exists) {
       let promotionData = promotionDetails.data()
       setLog(raw, state.FIND_CODE, promotionData)
